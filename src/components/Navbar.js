e01b0f9c10170b6d108bd5a6a9530693d7f94ebc
Neale Taylor <nealeptaylor@gmail.com>
2022-06-23 16:09:19 -0400

    add content to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 export default function Navbar() {
   return (
-    <h1>This is the Navbar</h1>
+    <div className="Navbar">
+      <div className="titleSection">
+        <img src="../../logo192.png" alt="react logo"></img>
+        <h3>ReactFacts</h3>
+      </div>
+      <h4>React Course- Project 1</h4>
+    </div>
   )
 }
